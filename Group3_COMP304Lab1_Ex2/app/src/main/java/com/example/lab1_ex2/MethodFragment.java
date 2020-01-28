5b2421e8422bfe7b98385dd75098c1ea9dc183fe
LesLie-14 <leslie.dizon.14@gmail.com>
2020-01-27 21:36:50 -0500

    Lab 2 Testing Fragments

---
+++ Group3_COMP304Lab1_Ex2/app/src/main/java/com/example/lab1_ex2/MethodFragment.java
@@ -0,0 +1,36 @@
+package com.example.lab1_ex2;
+
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MethodFragment extends Fragment {
+
+    private MethodViewModel mViewModel;
+
+    public static MethodFragment newInstance() {
+        return new MethodFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.method_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(MethodViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ Group3_COMP304Lab1_Ex2/app/src/main/java/com/example/lab1_ex2/MethodViewModel.java
@@ -0,0 +1,7 @@
+package com.example.lab1_ex2;
+
+import androidx.lifecycle.ViewModel;
+
+public class MethodViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}

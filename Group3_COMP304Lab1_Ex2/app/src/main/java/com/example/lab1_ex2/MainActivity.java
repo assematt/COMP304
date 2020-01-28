730d7e36924a1bc609b165bf069f8c41bb0584e7
LesLie-14 <leslie.dizon.14@gmail.com>
2020-01-28 02:14:32 -0500

    Lab1 Exercise 2 updates

--- Group3_COMP304Lab1_Ex2/app/src/main/java/com/example/lab1_ex2/MainActivity.java
+++ Group3_COMP304Lab1_Ex2/app/src/main/java/com/example/lab1_ex2/MainActivity.java
@@ -1,3 +1,9 @@
+/*
+ * Author: Group3
+ * Members: Matteo Assenza, Leslie Renee Dizon, Robin Joseph Augustine, Jokku Sabu Kattakkayam, Harkirat Grewal
+ * Date: January 27, 2020
+ * */
+
 package com.example.lab1_ex2;
 
 import androidx.appcompat.app.AppCompatActivity;
